50c87a1000c1cd98877d10ad8f4e721eedd674cd
Rashi <rashidixit129@gmail.com>
2022-12-31 20:33:33 +0530

    first commit

---
+++ vildy/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+
+import './App.css';
+import Movies from './components/movies';
+
+function App() {
+  return (
+    <>
+    <main className="container my-3 ">
+    <Movies/>
+    
+    </main>
+    </>
+  );
+}
+
+export default App;
